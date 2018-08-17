0fa7a5b8112c6f0b247cba1775c645a614e480e0
Erin Hogan <hogan10@charter.net>
2018-08-16 23:29:11 -0500

    Complete

---
+++ CG 6-1/CG 6-1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+
+namespace CG_6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ask the user for their first name and hold the data
+            Console.WriteLine("Please type your first name: ");
+            string firstName = Console.ReadLine();
+
+            //ask the user for their last name and hold the data
+            Console.WriteLine("Please type your last name: ");
+            string lastName = Console.ReadLine();
+
+            //call the method below to string together the data that you held to create the full name on screen
+            Console.WriteLine(GiveFullName(firstName, lastName));
+            Console.ReadLine();
+
+        }
+        /// <summary>
+        /// take the answer to first name and last name and make it one full name
+        /// </summary>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <returns>full name</returns>
+        private static string GiveFullName(string firstName, string lastName)
+        {
+            //stringBuilder allows you to create a string by taking up less memory
+            StringBuilder fullName = new StringBuilder();
+            //append adds to the copy of the string
+            fullName.Append(firstName);
+            fullName.Append(" ");
+            fullName.Append(lastName);
+
+            //convert full name value into a string
+            string GiveFullName = fullName.ToString();
+            //print out the full name to the screen
+            return GiveFullName;
+        }
+    }
+}
